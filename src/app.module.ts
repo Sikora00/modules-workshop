08f50b16f637503c4cd166caeebfad288b751824
Maciej Sikorski <maciejsikorski00@gmail.com>
2022-10-21 17:47:17 +0200

    Conditional persistance

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,20 @@ import { Module } from '@nestjs/common';
 import { LibraryController } from './api/library.controller';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { InMemoryBookInstances } from './persistance/in-memory/in-memory-book-instances';
 import { InMemoryPersistanceModule } from './persistance/in-memory/in-memory-persistance.module';
+import { PersistanceJsonFileModule } from './persistance/json-file/persistance-json-file.module';
 import { AddingBook } from './use-cases/adding-book';
 import { AddingBookInstance } from './use-cases/adding-book-instance';
 import { CancelingHold } from './use-cases/canceling-hold';
 import { PlacingOnHold } from './use-cases/placing-on-hold';
 
+const persistance =
+  process.env.ENV !== 'DEV'
+    ? PersistanceJsonFileModule
+    : InMemoryPersistanceModule;
+
 @Module({
-  imports: [InMemoryPersistanceModule],
+  imports: [persistance],
   controllers: [AppController, LibraryController],
   providers: [
     AppService,
@@ -18,7 +23,6 @@ import { PlacingOnHold } from './use-cases/placing-on-hold';
     AddingBookInstance,
     CancelingHold,
     PlacingOnHold,
-    InMemoryBookInstances,
   ],
 })
 export class AppModule {}
